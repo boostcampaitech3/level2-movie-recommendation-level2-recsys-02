5cd5619ee70b423b9cb19cc5839272714320764f
ParkByungYoon <21800271@handong.edu>
2022-03-25 01:14:17 +0000

    DeepFM

---
+++ DeepFM/dataset.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, Subset, random_split
+
+
+class RatingDataset(Dataset):
+    def __init__(self, data_dir):
+        data = pd.read_csv(data_dir + '/context-aware/ratings_with_genres.csv')
+
+        self.n_user = data.loc[:,'user'].nunique()
+        self.n_item = data.loc[:,'item'].nunique()
+        self.n_genre = data.loc[:,'genre'].nunique()
+
+        user_col = torch.tensor(data.loc[:,'user'])
+        item_col = torch.tensor(data.loc[:,'item'])
+        genre_col = torch.tensor(data.loc[:,'genre'])
+
+        offsets = [0, self.n_user, self.n_user+self.n_item]
+        for col, offset in zip([user_col, item_col, genre_col], offsets):
+            col += offset
+
+        self.X = torch.cat([user_col.unsqueeze(1), item_col.unsqueeze(1), genre_col.unsqueeze(1)], dim=1).long()
+        self.y = torch.tensor(list(data.loc[:,'rating'])).long()
+        self.data_len = len(data)
+
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
+
+    def __len__(self):
+        return self.data_len
+
+    def split_dataset(self, train_ratio=0.9) -> Tuple[Subset, Subset]:
+        train_size = int(train_ratio * self.data_len)
+        test_size = self.data_len - train_size
+        train_dataset, test_dataset = random_split(self, [train_size, test_size])
+        
+        return train_dataset, test_dataset
+
+    def get_input_dimensions(self):
+        return [self.n_user, self.n_item, self.n_genre]
+
+
+if __name__ == '__main__' :
+    dataset = RatingDataset('/opt/ml/movie-recommendation/data/train/context-aware/ratings_with_genres.csv')
+    print(len(dataset))
---
+++ DeepFM/train.py
@@ -0,0 +1,229 @@
+import argparse
+import glob
+import json
+import os
+import random
+import re
+from importlib import import_module
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from loss import create_criterion
+
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+def increment_path(path, exist_ok=False):
+    """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+
+    Args:
+        path (str or pathlib.Path): f"{model_dir}/{args.name}".
+        exist_ok (bool): whether increment path (increment if False).
+    """
+    path = Path(path)
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}*")
+        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]
+        n = max(i) + 1 if i else 2
+        return f"{path}{n}"
+
+
+def train(data_dir, model_dir, args):
+    seed_everything(args.seed)
+    save_dir = increment_path(os.path.join(model_dir, args.name))
+
+    # -- settings
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # -- dataset
+    dataset_module = getattr(import_module("dataset"), args.dataset)  # default: BaseAugmentation
+    dataset = dataset_module(
+        data_dir=data_dir,
+    )
+
+    # -- data_loader
+    train_set, val_set = dataset.split_dataset(1-args.val_ratio)
+
+    train_loader = DataLoader(
+        train_set,
+        batch_size=args.batch_size,
+        num_workers=4,
+        shuffle=True,
+        pin_memory=use_cuda,
+        drop_last=True,
+    )
+
+    val_loader = DataLoader(
+        val_set,
+        batch_size=args.valid_batch_size,
+        num_workers=4,
+        shuffle=False,
+        pin_memory=use_cuda,
+        drop_last=True,
+    )
+
+    # -- model
+    model_module = getattr(import_module("model"), args.model)
+    model = model_module(
+        input_dims = dataset.get_input_dimensions(),
+        embedding_dim = 10, 
+        mlp_dims = [30, 20, 10],
+    ).to(device)
+    model = torch.nn.DataParallel(model)
+
+    # -- loss & metric
+    criterion = create_criterion(args.criterion)
+    opt_module = getattr(import_module("torch.optim"), args.optimizer) 
+    optimizer = opt_module(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        lr=args.lr,
+    )
+    scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
+
+    # -- logging
+    logger = SummaryWriter(log_dir=save_dir)
+    with open(os.path.join(save_dir, 'config.json'), 'w', encoding='utf-8') as f:
+        json.dump(vars(args), f, ensure_ascii=False, indent=4)
+
+    best_val_acc = 0
+    best_val_loss = np.inf
+    counter = 0
+
+    patience = 5
+    accumulation_steps = 1
+    for epoch in range(args.epochs):
+        # train loop
+        model.train()
+        loss_value = 0
+        matches = 0
+        for idx, train_batch in enumerate(train_loader):
+            inputs, labels = train_batch
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outs = model(inputs)
+            preds = torch.round(outs)
+            loss = criterion(outs, labels.float())
+
+            loss.backward()
+            if (idx+1) % accumulation_steps == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+
+            loss_value += loss.item()
+            matches += (preds == labels).sum().item()
+            if (idx + 1) % args.log_interval == 0:
+                train_loss = loss_value / args.log_interval
+    
+                train_acc = matches / args.batch_size / args.log_interval
+                
+                current_lr = get_lr(optimizer)
+                print(
+                    f"Epoch[{epoch}/{args.epochs}]({idx + 1}/{len(train_loader)}) || "
+                    f"training loss {train_loss:4.4} || training accuracy {train_acc:4.2%} || lr {current_lr}"
+                )
+                logger.add_scalar("Train/loss", train_loss, epoch * len(train_loader) + idx)
+                logger.add_scalar("Train/accuracy", train_acc, epoch * len(train_loader) + idx)
+
+                loss_value = 0
+                matches = 0
+
+        scheduler.step()
+
+        # val loop
+        with torch.no_grad():
+            print("Calculating validation results...")
+            model.eval()
+            val_loss_items = []
+            val_acc_items = []
+            for val_batch in val_loader:
+                inputs, labels = val_batch
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                outs = model(inputs)
+                preds = torch.round(outs)
+
+                loss_item = criterion(outs, labels.float()).item()
+                acc_item = (labels == preds).sum().item()
+                val_loss_items.append(loss_item)
+                val_acc_items.append(acc_item)
+
+            val_loss = np.sum(val_loss_items) / len(val_loader)
+            val_acc = np.sum(val_acc_items) / len(val_set)
+            best_val_loss = min(best_val_loss, val_loss)
+            if val_acc > best_val_acc:
+                print("New best model for val accuracy! saving the model..")
+                torch.save(model.module.state_dict(), f"{save_dir}/best.pth")
+                best_val_acc = val_acc
+                counter = 0
+            else:
+                counter += 1
+            # Callback2: patience 횟수 동안 성능 향상이 없을 경우 학습을 종료시킵니다.
+            if counter > patience:
+                print("Early Stopping...")
+                break
+            print(
+                f"[Val] acc : {val_acc:4.2%}, loss: {val_loss:4.2} || "
+                f"best acc : {best_val_acc:4.2%}, best loss: {best_val_loss:4.2}"
+            )
+            logger.add_scalar("Val/loss", val_loss, epoch)
+            logger.add_scalar("Val/accuracy", val_acc, epoch)
+            print()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    from dotenv import load_dotenv
+    import os
+    load_dotenv(verbose=True)
+
+    # Data and model checkpoints directories
+    parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
+    parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train (default: 1)')
+    parser.add_argument('--dataset', type=str, default='RatingDataset', help='dataset augmentation type (default: MaskBaseDataset)')
+    parser.add_argument('--batch_size', type=int, default=1024, help='input batch size for training (default: 64)')
+    parser.add_argument('--valid_batch_size', type=int, default=512, help='input batch size for validing (default: 1000)')
+    parser.add_argument('--model', type=str, default='DeepFM', help='model type (default: DeepFM)')
+    parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer type (default: Adam)')
+    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
+    parser.add_argument('--val_ratio', type=float, default=0.1, help='ratio for validaton (default: 0.2)')
+    parser.add_argument('--criterion', type=str, default='binary_cross_entropy', help='criterion type (default: cross_entropy)')
+    parser.add_argument('--lr_decay_step', type=int, default=20, help='learning rate scheduler deacy step (default: 20)')
+    parser.add_argument('--log_interval', type=int, default=500, help='how many batches to wait before logging training status')
+    parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
+
+    # Container environment
+    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/movie-recommendation/data/train'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR', './model'))
+
+    args = parser.parse_args()
+    print(args)
+
+    data_dir = args.data_dir
+    model_dir = args.model_dir
+
+    train(data_dir, model_dir, args)
