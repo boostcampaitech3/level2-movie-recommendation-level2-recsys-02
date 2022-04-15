b1bfbead775a2c9de694762c36203f6472bd54da
ParkByungYoon <21800271@handong.edu>
2022-04-15 01:58:17 +0000

    Factorization Machines

---
+++ FM/dataset.py
@@ -0,0 +1,102 @@
+from typing import Tuple
+
+import json
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, Subset, random_split
+from scipy.sparse import dok_matrix
+
+def zero_based_mapping(data) :
+    train_df = pd.read_csv('/opt/ml/movie-recommendation/data/train/train_ratings.csv')
+
+    users = list(set(train_df.loc[:,'user']))
+    items =  list(set(train_df.loc[:, 'item']))
+
+    n_user = len(users)
+    n_item = len(items)
+
+    # user, item을 zero-based index로 mapping
+    if n_user-1 != max(users):
+        users_dict = {users[i]: i for i in range(len(users))}
+        data['user']  = data['user'].map(lambda x : users_dict[x])
+        
+    if n_item-1 != max(items):
+        items_dict = {items[i]: i for i in range(len(items))}
+        data['item']  = data['item'].map(lambda x : items_dict[x])
+    
+    return data, n_user, n_item
+
+class FMDataset(Dataset):
+    def __init__(self, data_path):
+        self.data = pd.read_csv(data_path)
+
+        self.data, _, _ = zero_based_mapping(self.data)
+        self.attributes = self.get_item_attributes()
+
+        self.X = torch.tensor(np.array(self.data.loc[:, ['user', 'item']])).long()
+        self.y = torch.tensor(np.array(self.data.loc[:, 'rating'])).long()
+
+    def __getitem__(self, index):
+        item_i = self.X[index, 1]
+        X = torch.cat([self.X[index], self.attributes[item_i]])
+        return X, self.y[index]
+
+    def __len__(self):
+        return len(self.data)
+
+    def split_dataset(self, train_ratio=0.9) -> Tuple[Subset, Subset]:
+        train_size = int(train_ratio * len(self.data))
+        test_size = len(self.data) - train_size
+        train_dataset, test_dataset = random_split(self, [train_size, test_size])
+        
+        return train_dataset, test_dataset
+    
+    def get_item_attributes(self):
+        data_dir = '/opt/ml/movie-recommendation/data/train/'
+
+        with open(data_dir+'item2attributes.json', 'r') as f:
+            item2attributes = json.load(f)
+
+        attributes = []
+
+        for item in range(6807):    
+            attribute = [0] * 18
+            now_attribute = item2attributes[str(item)]
+            for a in now_attribute[1:]:
+                attribute[a] = 1
+            attributes.append([now_attribute[0]]+attribute)
+        
+        return torch.tensor(attributes)
+
+
+class DeepFMTestDataset(Dataset):
+    def __init__(self, data_dir, data):
+        train_df = pd.read_csv(data_dir+'/train/context-aware/Ratings with Side-Information.csv')
+
+        self.n_year = train_df.loc[:,'year'].nunique()
+        self.n_genre = 18
+        
+        self.data, self.n_user, self.n_item = zero_based_mapping(data)
+
+        user_col = torch.tensor(np.array(self.data.loc[:,'user']))
+        item_col = torch.tensor(np.array(self.data.loc[:,'item']))
+        year_col = torch.tensor(np.array(self.data.loc[:,'year']))
+
+        multi_hot_cols = self.data.columns.difference(['user', 'item', 'year','rating'])
+        genre_col = torch.tensor(np.array(self.data.loc[:,multi_hot_cols]))
+
+        offsets = [0, self.n_user, self.n_user+self.n_item]
+        for col, offset in zip([user_col, item_col, year_col], offsets):
+            col += offset
+
+        self.X = torch.cat([user_col.unsqueeze(1), item_col.unsqueeze(1), year_col.unsqueeze(1), genre_col], dim=1).long()
+    
+    def __getitem__(self, index):
+        return self.X[index]
+    
+    def __len__(self):
+        return len(self.data)
+
+    def get_input_dimensions(self):
+        return [self.n_user, self.n_item, self.n_year, self.n_genre]
---
+++ FM/loss.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+_criterion_entrypoints = {
+    'cross_entropy': nn.CrossEntropyLoss,
+    'binary_cross_entropy' : nn.BCELoss,
+}
+
+
+def criterion_entrypoint(criterion_name):
+    return _criterion_entrypoints[criterion_name]
+
+
+def is_criterion(criterion_name):
+    return criterion_name in _criterion_entrypoints
+
+
+def create_criterion(criterion_name, **kwargs):
+    if is_criterion(criterion_name):
+        create_fn = criterion_entrypoint(criterion_name)
+        criterion = create_fn(**kwargs)
+    else:
+        raise RuntimeError('Unknown loss (%s)' % criterion_name)
+    return criterion
